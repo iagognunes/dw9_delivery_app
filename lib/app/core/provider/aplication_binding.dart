888f5a600fab7b66881edd5a3da62a79d162d5f7
Iago Guedelha Nunes <iagognunes@outlook.com>
2023-02-03 16:03:48 -0300

    Aula 02
    Bloc;
    Helpers;
    Extensions;
    Providers;
    Usando a API;
    Tile do produto;
    Finalizando SplashPage.

---
+++ lib/app/core/provider/aplication_binding.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dw9_delivery_app/app/core/rest_client/custom_dio.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AplicationBinding extends StatelessWidget {
+  final Widget child;
+
+  const AplicationBinding({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [Provider(create: (context) => CustomDio())],
+      child: child,
+    );
+  }
+}
